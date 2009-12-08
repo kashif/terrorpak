d8a55778e575bfb56339e53648187d1922e41f20
Kashif Rasul <kashif@nomad-labs.com>
2009-12-08 01:17:09 +0100

    Added an Event resourse with google map view as root view.

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,8 @@
+class EventsController < ApplicationController
+  
+  def index
+    respond_to do |format|
+      format.html
+    end
+  end
+end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+var map;
+
+function initialize() {
+  var latlng = new google.maps.LatLng(30.18, 71.52);
+  var myOptions = {
+    zoom: 6,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+}
+
+window.onload = initialize;
