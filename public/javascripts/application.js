8f3f0d33e5b6a09d88e07969ac2148a55190f135
Kashif Rasul <kashif@nomad-labs.com>
2009-12-09 23:21:44 +0100

    Added simile timeplot widget

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,5 @@
 var map;
+var timeplot;
 
 function initialize() {
   var latlng = new google.maps.LatLng(30.18, 71.52);
@@ -7,7 +8,28 @@ function initialize() {
     center: latlng,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
+  
+  var plotInfo = [
+    Timeplot.createPlotInfo({
+      id: "plot1"
+    })
+  ];
+  
+  timeplot = Timeplot.create(document.getElementById("timeplot_canvas"), plotInfo);
   map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 }
 
+var resizeTimerID = null;
+
+function handleResize() {
+  if (resizeTimerID == null) {
+    resizeTimerID = window.setTimeout(function() {
+      resizeTimerID = null;
+      timeplot.repaint();
+    }, 100);
+  }
+}
+
 window.onload = initialize;
+window.onunload = GUnload;
+window.onresize = handleResize;
